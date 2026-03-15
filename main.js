dc96647f4d79152eabe24c8a918b8f5828aace18
awnupp <kcanup372@gmail.com>
2026-03-15 18:04:32 +0545

    Optimized the code

--- main.js
+++ main.js
@@ -1,45 +1,12 @@
-// Select buttons
-const greenBtn = document.getElementById("green");
-const redBtn = document.getElementById("red");
-const blueBtn = document.getElementById("blue");
-const randomBtn = document.getElementById("random");
+const body = document.getElementsByTagName("body")[0]
 
-// Green
-greenBtn.addEventListener("click", function () {
-  document.body.style.background = "green";
-});
+function setColor(Name){
+  body.style.backgroundColor = Name;
+}
 
-// Red
-redBtn.addEventListener("click", function () {
-  document.body.style.background = "red";
-});
-
-// Blue
-blueBtn.addEventListener("click", function () {
-  document.body.style.background = "blue";
-});
-
-// Random (solid or gradient)
-randomBtn.addEventListener("click", function () {
-  const isGradient = Math.random() > 0.5;
-
-  if (isGradient) {
-    // Random gradient
-    const color1 = getRandomColor();
-    const color2 = getRandomColor();
-    document.body.style.background = `linear-gradient(45deg, ${color1}, ${color2})`;
-  } else {
-    // Random solid color
-    document.body.style.background = getRandomColor();
-  }
-});
-
-// Function to generate random hex color
-function getRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+function randomColor(){
+  const red   = Math.round(Math.random()*255);
+  const blue  = Math.round(Math.random()*255);
+  const green = Math.round(Math.random()*255);
+  body.style.backgroundColor =  `rgb(${red},${blue},${green})`;
 }
